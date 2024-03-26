b31c076cf6762248ecc32dd162d010ad137174ab
wazeka <wazeka0412@gmail.com>
2024-03-26 17:02:38 +0900

    [fix: acquire_python_gil]

--- commune/modules/thread/src/lib.rs
+++ commune/modules/thread/src/lib.rs
@@ -1,8 +1,37 @@
 use pyo3::prelude::*;
+use std::thread;
 
 #[pyfunction]
-fn create_thread(py: Python, python_function: PyObject) -> PyResult<PyObject> {
-    Ok(python_function.call0(py)?.to_object(py))
+fn create_thread(_py: Python, python_function: PyObject) -> PyResult<()> {
+    println!("Start Thread Execution");
+
+    Python::with_gil(|_py| -> PyResult<()> {
+        python_function.call0(_py);
+        Ok(())
+    });
+
+
+    // let handle = thread::spawn(move || {
+    //     println!("start thread");
+
+    //     // Acquire the GIL within the thread
+    //     Python::with_gil(|py| -> PyResult<()> {
+    //         println!("Hello World");
+
+    //         // // Call the Python function using the provided PyObject
+    //         // match python_function.call0(py) {
+    //         //     Ok(_) => println!("Python function execution completed"),
+    //         //     Err(err) => eprintln!("Error calling Python function: {:?}", err),
+    //         // }
+
+    //         Ok(())
+    //     }).expect("Error acquiring GIL");
+    // });
+
+    // handle.join().expect("Thread panicked");
+
+    println!("Thread completed");
+    Ok(())
 }
 
 #[pymodule]
