5d82a53e5ebd2da656ba35ce3979ac923fb9ee4d
wazeka <wazeka0412@gmail.com>
2024-03-26 23:11:13 +0900

    [feat: execute_python_function_with_args_using_gil]

--- commune/modules/thread/Test.py
+++ commune/modules/thread/Test.py
@@ -1,10 +1,10 @@
 import rust_thread_executor
 
-def sum_fn():
+def sum_fn(start, end):
     sum_value = 0
-    for i in range(1, 100000001):
+    for i in range(start, end):
         sum_value += i
     print(sum_value)
 
-A = rust_thread_executor.create_thread(sum_fn)
+A = rust_thread_executor.create_thread(sum_fn, (1, 101))
 print("here => ", A)
--- commune/modules/thread/src/lib.rs
+++ commune/modules/thread/src/lib.rs
@@ -1,10 +1,13 @@
 use pyo3::prelude::*;
 use std::thread;
+use pyo3::types::PyTuple;
 
 #[pyfunction]
-fn execute_python_function(python_function: PyObject) -> PyResult<()> {
+fn execute_python_function(python_function: PyObject, args: Vec<PyObject>) -> PyResult<()> {
     Python::with_gil(|py| {
-        match python_function.call0(py) {
+        let args_tuple = PyTuple::new(py, args);
+
+        match python_function.call1(py, args_tuple) {
             Ok(_) => println!("Python function execution completed"),
             Err(err) => eprintln!("Error calling Python function: {:?}", err),
         }
@@ -14,11 +17,11 @@ fn execute_python_function(python_function: PyObject) -> PyResult<()> {
 }
 
 #[pyfunction]
-fn create_thread(_py: Python, python_function: PyObject) -> PyResult<()> {
+fn create_thread(_py: Python, python_function: PyObject, args: Vec<PyObject>) -> PyResult<()> {
     println!("Start Thread Execution");
 
     let handle = thread::spawn(|| {
-        execute_python_function(python_function).expect("Error calling Python function");
+        execute_python_function(python_function, args).expect("Error calling Python function");
     });
 
     println!("Thread completed");
