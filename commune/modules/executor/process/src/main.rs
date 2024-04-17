40c3be76e644de50d3ad14a34bfc668323a4daf6
wazeka <wazeka0412@gmail.com>
2024-04-17 16:00:52 +0900

    add_binary_crate

--- commune/modules/executor/process/src/main.rs
+++ commune/modules/executor/process/src/main.rs
@@ -1,9 +1,17 @@
 use rayon::prelude::*;
+use std::time::Instant;
 
-fn main() {    
-    let nums = vec![1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-    let _nums_squared: Vec<i32> = nums
+fn main() {
+    let start_time = Instant::now();
+    println!("Rayon Start");
+    let nums: Vec<u128> = (1..=100_000_000).collect();
+    let _nums_squared: u128 = nums
         .par_iter()
         .map(|&x| x * x)
-        .collect();
+        .sum();
+    
+    println!("Rayon End");
+    let end_time = Instant::now();
+    let elapsed_time = end_time - start_time;
+    println!("Elapsed time: {} milliseconds", elapsed_time.subsec_millis());
 }
